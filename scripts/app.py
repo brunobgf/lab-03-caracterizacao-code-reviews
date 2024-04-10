40de6aea4c0e905698c51be4018340af4d4aa53c
Marcio Lucas <48500194+MarcioLucas22@users.noreply.github.com>
2024-04-10 13:03:41 -0300

    feat: review duration

--- scripts/app.py
+++ scripts/app.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 import requests
 import json
+from datetime import datetime, timedelta
 
 def run_query(query, headers):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
@@ -9,6 +10,22 @@ def run_query(query, headers):
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, request.text))
+    
+
+def is_review_duration_greater_than_one_hour(review_created_at, pr_merged_at, pr_closed_at):
+    review_created_at = datetime.strptime(review_created_at, '%Y-%m-%dT%H:%M:%SZ')
+    pr_merged_at = datetime.strptime(pr_merged_at, '%Y-%m-%dT%H:%M:%SZ') if pr_merged_at else None
+    pr_closed_at = datetime.strptime(pr_closed_at, '%Y-%m-%dT%H:%M:%SZ') if pr_closed_at else None
+    
+    if pr_merged_at:
+        time_difference = pr_merged_at - review_created_at
+    elif pr_closed_at:
+        time_difference = pr_closed_at - review_created_at
+    else:
+        return False
+    
+    return time_difference > timedelta(hours=1)
+
 
 
 def search_repositories(end_cursor, headers):
@@ -40,13 +57,6 @@ def search_repositories(end_cursor, headers):
     """
 
     return run_query(query, headers)
-
-# result = search_repositories('null')['data']['search']
-# repositories = []
-# repositories.extend(list(map(lambda x: x['node'], result['edges'])))
-# print(json.dumps(repositories, indent=2))#repositories[0]["nameWithOwner"]
-# print('Fim')
-# input()
 
 
 def get_pull_requests(owner, repo_name, headers):
@@ -96,15 +106,12 @@ if __name__ == "__main__":
             
             pull_requests_data = get_pull_requests(*repo_name_with_owner.split('/'), headers)
             
-            for pr_node in pull_requests_data['data']['repository']['pullRequests']['nodes']:
-                print(json.dumps(pr_node, indent=3))
-                input()
-                # input()
-                if int(pr_node['reviews']['totalCount']) > 0:
-                    print('entrei')
+            for pr_node in pull_requests_data['data']['repository']['pullRequests']['nodes']:                
+                if int(pr_node['reviews']['totalCount']) > 0 and is_review_duration_greater_than_one_hour(pr_node['createdAt'], pr_node['mergedAt'], pr_node['closedAt']):
                     data.append({
-                        'pr_number': pr_node['number'],
-                        'pr_title': pr_node['title'],
+                        'repo': repo_edge['nameWithOwner'],
+                        # 'pr_number': pr_node['number'],
+                        'pr_description': pr_node['title'],
                         'pr_reviews_count': pr_node['reviews']['totalCount'],
                         'index': index
                     })
