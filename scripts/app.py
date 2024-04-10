37dd70240688086b2ae115a637b64ca9b0d3ab70
Marcio Lucas <48500194+MarcioLucas22@users.noreply.github.com>
2024-04-10 16:08:56 -0300

    almost finished

--- scripts/app.py
+++ scripts/app.py
@@ -3,6 +3,8 @@ import dotenv
 import requests
 import json
 from datetime import datetime, timedelta
+import pandas as pd
+
 
 def run_query(query, headers):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
@@ -27,6 +29,18 @@ def is_review_duration_greater_than_one_hour(review_created_at, pr_merged_at, pr
     return time_difference > timedelta(hours=1)
 
 
+def calculate_pr_interval(pr_created_at, pr_merged_at, pr_closed_at):
+  pr_created_at = datetime.strptime(pr_created_at, '%Y-%m-%dT%H:%M:%SZ') 
+  pr_merged_at = datetime.strptime(pr_merged_at, '%Y-%m-%dT%H:%M:%SZ') if pr_merged_at else None
+  pr_closed_at = datetime.strptime(pr_closed_at, '%Y-%m-%dT%H:%M:%SZ') if pr_closed_at else None
+
+  
+  last_activity_time = max(pr_merged_at, pr_closed_at)
+
+  interval = last_activity_time - pr_created_at
+
+  return interval
+
 
 def search_repositories(end_cursor, headers):
     query = """
@@ -46,6 +60,7 @@ def search_repositories(end_cursor, headers):
           node {
             ... on Repository {
               nameWithOwner
+              stargazerCount
               pullRequests(states: [MERGED, CLOSED], first: 100) {
                 totalCount
               }
@@ -66,14 +81,23 @@ def get_pull_requests(owner, repo_name, headers):
         pullRequests(states: [MERGED, CLOSED], first: 100) {
           nodes {
             ... on PullRequest {
+              comments {
+                  totalCount
+              }
               number
-              title
+              title 
               reviews(first: 1) {
-                totalCount              
+                totalCount             
               }
               mergedAt
               closedAt
               createdAt
+              bodyText
+              participants(first: 100) {
+              nodes {
+                  login
+                }
+              }
             }
           }
         }
@@ -83,38 +107,115 @@ def get_pull_requests(owner, repo_name, headers):
 
     return run_query(query, headers)
 
-if __name__ == "__main__":
-    index = 1
-    data = []
-    end_cursor = "null"
-    num_repos = 200
-    while len(data) < num_repos:
-        dotenv.load_dotenv()
-        headers = {"Authorization": "Bearer " + os.environ['API_TOKEN']}
 
-        try:
-            repositories_data = search_repositories(end_cursor, headers)['data']['search']
-        except:
-            continue
+def get_repository_commit_stats(owner, repo_name, headers):
+    query = """
+    {
+      repository(owner: "%s", name: "%s") {
+        defaultBranchRef {
+          target {
+            ... on Commit {
+              history(first: 100) {
+                edges {
+                  node {
+                    additions
+                    deletions
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """ % (owner, repo_name)
 
-        end_cursor = "\"" + repositories_data["pageInfo"]["endCursor"] + "\"" if repositories_data["pageInfo"]["endCursor"] is not None else "null"
-        repositories = []
-        repositories.extend(list(map(lambda x: x['node'], repositories_data['edges'])))
+    return run_query(query, headers)
 
-        for repo_edge in repositories:
-            repo_name_with_owner = repo_edge['nameWithOwner']
-            
-            pull_requests_data = get_pull_requests(*repo_name_with_owner.split('/'), headers)
-            
-            for pr_node in pull_requests_data['data']['repository']['pullRequests']['nodes']:                
-                if int(pr_node['reviews']['totalCount']) > 0 and is_review_duration_greater_than_one_hour(pr_node['createdAt'], pr_node['mergedAt'], pr_node['closedAt']):
-                    data.append({
-                        'repo': repo_edge['nameWithOwner'],
-                        # 'pr_number': pr_node['number'],
-                        'pr_description': pr_node['title'],
-                        'pr_reviews_count': pr_node['reviews']['totalCount'],
-                        'index': index
-                    })
-                    index +=1
-                
-    print(json.dumps(data, indent=1))
+
+def get_repository_files(owner, repo_name, headers):
+    query = """
+    {
+      repository(owner: "%s", name: "%s") {
+        object(expression: "HEAD:") {
+          ... on Tree {
+            entries {
+              name
+            }
+          }
+        }
+      }
+    }
+    """ % (owner, repo_name)
+
+    return run_query(query, headers)
+
+
+dotenv.load_dotenv()
+headers = {"Authorization": "Bearer " + os.environ['API_TOKEN']}
+
+index = 1
+repos = []
+end_cursor = "null"
+num_repos = 200
+while len(repos) < num_repos:
+    try:
+        repositories_data = search_repositories(end_cursor, headers)['data']['search']
+    except:
+        continue
+
+    end_cursor = "\"" + repositories_data["pageInfo"]["endCursor"] + "\"" if repositories_data["pageInfo"]["endCursor"] is not None else "null"
+    repositories = []
+    repositories.extend(list(map(lambda x: x['node'], repositories_data['edges'])))
+
+    for repo in repositories:
+      total_reviews_pr = 0
+      repo_name_with_owner = repo['nameWithOwner']
+      count_pr_repo = repo['pullRequests']['totalCount']
+
+      pull_requests = get_pull_requests(*repo_name_with_owner.split('/'), headers)['data']['repository']['pullRequests']['nodes']
+
+      for pr in pull_requests:          
+        if int(pr['reviews']['totalCount']) > 0 and is_review_duration_greater_than_one_hour(pr['createdAt'], pr['mergedAt'], pr['closedAt']):
+          total_reviews_pr += int(pr['reviews']['totalCount'])
+
+      if total_reviews_pr > 0:
+
+        grand_total_rows_added_and_removes = 0
+        total_rows_added = 0
+        total_rows_removed = 0
+
+        for commit in get_repository_commit_stats(*repo_name_with_owner.split('/'), headers)['data']['repository']['defaultBranchRef']['target']['history']['edges']:
+          total_rows_added += commit['node']['additions']
+          total_rows_removed += commit['node']['deletions']
+          grand_total_rows_added_and_removes += total_rows_added + total_rows_removed
+
+        total_repository_files = get_repository_files(*repo_name_with_owner.split('/'), headers)['data']['repository']['object']['entries']
+        # print(json.dumps(pull_requests[index - 1], indent=3))
+        repos.append({
+          'repo_name_with_owner': repo['nameWithOwner'],
+          'stars': repo['stargazerCount'],
+          'total_pr': repo['pullRequests']['totalCount'],
+          'total_pr_reviews': total_reviews_pr,
+          'total_repository_files': total_repository_files,
+          # 'analysis_time': calculate_pr_interval(pr['createdAt'], pr['mergedAt'], pr['closedAt']),
+          'number_characters_description': len(list(pr['bodyText'])),
+          'number_participants': len(pr['participants']['nodes']),
+          'total_comments_pr': pr['comments']['totalCount'],
+          'total_rows_added': total_rows_added,
+          'total_rows_removed': total_rows_removed,
+          'grand_total_rows_added_and_removed': grand_total_rows_added_and_removes,
+          'index': index
+        })
+        index +=1
+
+print(json.dumps(repos, indent=1))
+
+df = pd.DataFrame(data=repos)
+
+if not os.path.exists('./dataset'):
+  os.mkdir('./dataset')
+
+df.to_csv('./scripts/dataset/repos.csv', index=False)
+
+print('Finished')
